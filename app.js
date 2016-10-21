c8c5d7a20f9c566223326b7c4904b2cad837382c
Fred <delon.frederic.pro.job@gmail.com>
2016-10-21 12:31:03 +0200

    Step 3 js btn reset pop

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
-var localLocale = moment();
-localLocale.locale('fr');
+
 	var app = {
 		jour:null,
 		année:null,
@@ -8,16 +7,29 @@ localLocale.locale('fr');
 		result:null,
 
 		init: function(){
+			moment.locale('fr');
 			this.listener();
 
 		},
-
 		listener: function(){
 			$('#vérifier').on('click',app.whatAday);
+			$('#restart').on('click',app.init);
+
 		},
 		day: function(){
 			app.jour = $('#days').val();
-			
+			if(app.jour <1){
+				$('#head').append('<div id="message" class="error">Try again</div>');
+				$('#days').css('border','red solid 3px');
+				app.init();
+			}
+			if(app.jour >31){
+				$('#days').css('border','red solid 3px');
+				app.init();
+			} else {
+				$('#reset').hide()
+				$('body').append('<div class="overlay"><h1 id="whatAday"></h1><button id="restart" label="Recommencer">Restart</button></div>');
+			}
 		},
 		month: function(){
 			app.mois = $('#month').val();
@@ -25,12 +37,15 @@ localLocale.locale('fr');
 		},
 		year: function(){
 			app.année = $('#year').val();
+			if(app.année <1){
+				$('#year').css('border','red solid 3px');
+				app.init();
+			}
 			
 		},
 		vérif: function(){
 			app.result = moment({year:app.année, month: app.mois, day: app.jour}).format('dddd');
-			
-			
+
 		},
 		whatAday: function(){
 			app.day();
@@ -39,13 +54,14 @@ localLocale.locale('fr');
 			app.vérif();
 			$('#whatAday').text(app.result);
 
-
+		},
+		reset: function(){
+			$('#reset').show()
+			$('.overlay').hide()
+			app.init();
 		}
-
 	};
-	moment.locale('fr');
 	app.init();
-
 
 });
 
